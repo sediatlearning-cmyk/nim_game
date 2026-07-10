99006b6f12b5f7cff75f72ae59f518f2ab166789
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-10 14:54:41 +0200

    creation of game variables

---
+++ nim_game.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*-coding: utf-8-*-
+
+# ========================================================
+#                       methods
+# ========================================================
+
+
+# ========================================================
+#                         main
+# ========================================================
+if __name__ == '__main__':
+
+    is_game_finished = False
+    is_turn_finished = False
+    matches_to_remove = 0
+    matches = 21
+    MESSAGE = "Which matches do you want to remove?"
+    last_match = False
+    gamer1_name = input("Gamer1 :Enter your name: ")
+    gamer2_name = input("Gamer2 :Enter your name: ")
+    gamer_start = input("who start the game ? (your name)")
+
+
