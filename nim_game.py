cd6f9a450989b54855a829be7f8903427a2999be
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-13 16:02:06 +0200

    debug check_matches_to_remove_under_number_of_remaining_matches

--- nim_game.py
+++ nim_game.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 # -*-coding: utf-8-*-
 
+
 # ========================================================
 #                       methods
 # ========================================================
+
 
 def turn_finished():
     global is_turn_finished
     # turn  doesn't finish if number_of_remaining_matches not equal to 0
     if number_of_remaining_matches != 0:
         is_turn_finished = False
+
+
+def calculate_number_of_matches_by_turn(removes_matches: int, total_count_of_matches_after_turn):
+    global is_turn_finished
+    # calcul du nombre d'allumettes restantes
+    total_count_of_matches_after_turn = total_count_of_matches_after_turn - removes_matches
+    print(f"There are {total_count_of_matches_after_turn} matches left.")
+    if total_count_of_matches_after_turn < 0:
+        is_turn_finished = True
+        return total_count_of_matches_after_turn
+    return total_count_of_matches_after_turn
 
 
 def turn_of_the_gamer(total_count_of_matches_after_turn):
@@ -22,24 +35,33 @@ def turn_of_the_gamer(total_count_of_matches_after_turn):
         while not (1 <= matches_to_remove <= 4):
             matches_to_remove = int(input(MESSAGE))
 
-        # calcul du nombre d'allumettes restantes
-        total_count_of_matches_after_turn = total_count_of_matches_after_turn - matches_to_remove
-        print(f"There are {total_count_of_matches_after_turn} matches left.")
-        is_turn_finished = True
+        total_count_of_matches_after_turn = (
+            calculate_number_of_matches_by_turn(
+                matches_to_remove,
+                total_count_of_matches_after_turn))
         return total_count_of_matches_after_turn
     return total_count_of_matches_after_turn
 
 
-def check_matches_to_remove_under_number_of_remaining_matches():
+def check_matches_to_remove_under_number_of_remaining_matches(total_count_of_matches_after_turn):
     global matches_to_remove
-    while matches_to_remove > number_of_remaining_matches:
-        print("You cannot remove more matches than there are.")
-        matches_to_remove = int(input(MESSAGE))
+    number_of_matches_to_remain = total_count_of_matches_after_turn
+    if matches_to_remove > total_count_of_matches_after_turn:
+        print("The only digit you could write is 1, so:")
+        if 2 <= matches_to_remove <= 3:
+            matches_to_remove = int(input(MESSAGE))
+            number_of_matches_to_remain = calculate_number_of_matches_by_turn(
+             matches_to_remove,
+             total_count_of_matches_after_turn)
+
+        if 0 >= number_of_matches_to_remain >= 1:
+            print("The game is finished")
+            game_is_finished()
 
 
 def game_is_finished():
     global is_game_finished
-    if number_of_remaining_matches == 0:
+    if number_of_remaining_matches <= 1:
         print("You lose")
         is_game_finished = True
         return is_game_finished
@@ -62,26 +84,26 @@ if __name__ == '__main__':
     last_match = False
 
     print("Welcome to NIM Game")
-    gamer1_name = input("Gamer1 :Enter your name: ")
-    gamer2_name = input("Gamer2 :Enter your name: ")
-    gamer_start = input("who start the game ? (your name)")
+    gamer1_name = input("Gamer1 : Enter your name: ")
+    gamer2_name = input("Gamer2 : Enter your name: ")
+    gamer_start = input("Who start the game ? (your name) : ")
 
     while not is_game_finished:  # tant que la partie n'est pas terminé
-        if gamer_start == gamer1_name:  # si le nom du joueur est celui qui commence
+        if gamer_start == gamer1_name:  # si le nom du joueur est égal à celui qui commence
             # tour du joueur 1
             turn_finished()
-            print("Player 1's turn")
+            print(f"Player {gamer1_name}, it's your turn")
             number_of_remaining_matches = turn_of_the_gamer(number_of_remaining_matches)
-            check_matches_to_remove_under_number_of_remaining_matches()
+            check_matches_to_remove_under_number_of_remaining_matches(number_of_remaining_matches)
             is_game_finished = game_is_finished()
             gamer_start = gamer2_name
 
         else:
             if gamer_start == gamer2_name:  # tour du joueur 2
-                print("Player 2's turn")
+                print(f"Player {gamer2_name}, it's your turn")
                 turn_finished()
                 number_of_remaining_matches = turn_of_the_gamer(number_of_remaining_matches)
-                check_matches_to_remove_under_number_of_remaining_matches()
+                check_matches_to_remove_under_number_of_remaining_matches(number_of_remaining_matches)
                 is_game_finished = game_is_finished()
                 gamer_start = gamer1_name
 
